d6455fed75ac5a53da754b6b5560b5788c50ed88
Ryan Earl <ryanleoearl@gmail.com>
2020-10-18 23:53:13 -0700

    Completed another test

--- test_hash_table.py
+++ test_hash_table.py
@@ -108,37 +108,37 @@ class TestHashTable(unittest.TestCase):
         h = HashTable(3)
         self.assertEqual(None, h['foo'])
 
-    # def test_retrieve_one(self):
-    #     """
-    #     The value of an inserted k-v pair is retrievable.
-    #     """
-    #     h = HashTable(3)
-    #     h['foo'] = 'bar'
-    #     self.assertEqual('bar', h['foo'])
+    def test_retrieve_one(self):
+        """
+        The value of an inserted k-v pair is retrievable.
+        """
+        h = HashTable(3)
+        h['foo'] = 'bar'
+        self.assertEqual('bar', h['foo'])
 
-    # """
-    # Insertion
-    # """
+    """
+    Insertion
+    """
 
-    # def test_insert_two(self):
-    #     """
-    #     Inserting two k-v pairs stores them as two-element arrays in the list
-    #     at the right index.
-    #     """
-    #     h = HashTable(3)
-    #     h[9] = 'foo' # Using numbers as keys for visibility.
-    #     h[11] = 'bar'
-    #     self.assertEqual([[9, 'foo']], h.data[0])
-    #     self.assertEqual([[11, 'bar']], h.data[2])
+    def test_insert_two(self):
+        """
+        Inserting two k-v pairs stores them as two-element arrays in the list
+        at the right index.
+        """
+        h = HashTable(3)
+        h[9] = 'foo' # Using numbers as keys for visibility.
+        h[11] = 'bar'
+        self.assertEqual([[9, 'foo']], h.data[0])
+        self.assertEqual([[11, 'bar']], h.data[2])
 
-    # def test_insert_existing(self):
-    #     """
-    #     Inserting a k-v pair where the key already exists overwrites the old value.
-    #     """
-    #     h = HashTable(3)
-    #     h[9] = 'foo' # Using numbers as keys for visibility.
-    #     h[9] = 'bar'
-    #     self.assertEqual([[9, 'bar']], h.data[0])
+    def test_insert_existing(self):
+        """
+        Inserting a k-v pair where the key already exists overwrites the old value.
+        """
+        h = HashTable(3)
+        h[9] = 'foo' # Using numbers as keys for visibility.
+        h[9] = 'bar'
+        self.assertEqual([[9, 'bar']], h.data[0])
 
     # def test_insert_collision(self):
     #     """
